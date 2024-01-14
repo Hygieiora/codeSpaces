ba6010a2eab5cd6d2316957f5380a82862c750cc
FoM <137290068+TravisBoltz@users.noreply.github.com>
2024-01-14 06:52:36 +0000

    therapy and header and sidebar components

---
+++ app/components/sidebar.js
@@ -0,0 +1,136 @@
+export default function SideBar() {
+  return (
+    <div className="hidden md:block h-full fixed md-hidden sm-hidden bg-white p-3 space-y-2 w-60 dark:bg-gray-900 dark:text-gray-100">
+      <div className="flex items-center p-2 space-x-4">
+        <img
+          src="https://source.unsplash.com/100x100/?portrait"
+          alt=""
+          className="w-8 h-8 rounded-full dark:bg-gray-500"
+        />
+        <div>
+          <h2 className="text-xs font-semibold">Handsome Musah</h2>
+          <span className="flex items-center space-x-1">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="text-xs hover:underline dark:text-gray-400"
+            >
+              View profile
+            </a>
+          </span>
+        </div>
+      </div>
+      <div className="divide-y justify-between dark:divide-gray-700">
+        <ul className="pt-2 pb-4 space-y-1 text-sm">
+          <li>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center p-2 space-x-3 rounded-md"
+            >
+              <img src="therapy.png" className="w-6 h-6" />
+
+              <span>Therapists</span>
+            </a>
+          </li>
+          <li className="dark:bg-gray-800 dark:text-gray-50">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center p-2 space-x-3 rounded-md"
+            >
+              <img src="community.png" className="w-6 h-6" />
+              <span>Community</span>
+            </a>
+          </li>
+          <li className="dark:bg-gray-800 dark:text-gray-50">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center p-2 space-x-3 rounded-md"
+            >
+              <img src="calender.png" className="w-6 h-6" />
+              <span>Appointments</span>
+            </a>
+          </li>
+          <li className="dark:bg-gray-800 dark:text-gray-50">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center p-2 space-x-3 rounded-md"
+            >
+              <img src="messages.png" className="w-6 h-6" />
+              <span>Messages</span>
+            </a>
+          </li>
+          <li className="dark:bg-gray-800 dark:text-gray-50">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center p-2 space-x-3 rounded-md"
+            >
+              <img src="books.png" className="w-6 h-6" />
+              <span>Books</span>
+            </a>
+          </li>
+          <li>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center p-2 space-x-3 rounded-md"
+            >
+              <span>Privacy</span>
+            </a>
+          </li>
+          <li className="dark:bg-gray-800 dark:text-gray-50">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center p-2 space-x-3 rounded-md"
+            >
+              <span>Terms and conditions</span>
+            </a>
+          </li>
+        </ul>
+        <div className="fixed bottom-0 pb-2 space-y-1 text-sm bg-white">
+          <ul className=" pb-2 space-y-1 text-sm">
+            <li>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                className="flex items-center p-2 space-x-3 rounded-md"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  className="w-5 h-5 fill-current dark:text-gray-400"
+                >
+                  <path d="M245.151,168a88,88,0,1,0,88,88A88.1,88.1,0,0,0,245.151,168Zm0,144a56,56,0,1,1,56-56A56.063,56.063,0,0,1,245.151,312Z"></path>
+                  <path d="M464.7,322.319l-31.77-26.153a193.081,193.081,0,0,0,0-80.332l31.77-26.153a19.941,19.941,0,0,0,4.606-25.439l-32.612-56.483a19.936,19.936,0,0,0-24.337-8.73l-38.561,14.447a192.038,192.038,0,0,0-69.54-40.192L297.49,32.713A19.936,19.936,0,0,0,277.762,16H212.54a19.937,19.937,0,0,0-19.728,16.712L186.05,73.284a192.03,192.03,0,0,0-69.54,40.192L77.945,99.027a19.937,19.937,0,0,0-24.334,8.731L21,164.245a19.94,19.94,0,0,0,4.61,25.438l31.767,26.151a193.081,193.081,0,0,0,0,80.332l-31.77,26.153A19.942,19.942,0,0,0,21,347.758l32.612,56.483a19.937,19.937,0,0,0,24.337,8.73l38.562-14.447a192.03,192.03,0,0,0,69.54,40.192l6.762,40.571A19.937,19.937,0,0,0,212.54,496h65.222a19.936,19.936,0,0,0,19.728-16.712l6.763-40.572a192.038,192.038,0,0,0,69.54-40.192l38.564,14.449a19.938,19.938,0,0,0,24.334-8.731L469.3,347.755A19.939,19.939,0,0,0,464.7,322.319Zm-50.636,57.12-48.109-18.024-7.285,7.334a159.955,159.955,0,0,1-72.625,41.973l-10,2.636L267.6,464h-44.89l-8.442-50.642-10-2.636a159.955,159.955,0,0,1-72.625-41.973l-7.285-7.334L76.241,379.439,53.8,340.562l39.629-32.624-2.7-9.973a160.9,160.9,0,0,1,0-83.93l2.7-9.972L53.8,171.439l22.446-38.878,48.109,18.024,7.285-7.334a159.955,159.955,0,0,1,72.625-41.973l10-2.636L222.706,48H267.6l8.442,50.642,10,2.636a159.955,159.955,0,0,1,72.625,41.973l7.285,7.334,48.109-18.024,22.447,38.877-39.629,32.625,2.7,9.972a160.9,160.9,0,0,1,0,83.93l-2.7,9.973,39.629,32.623Z"></path>
+                </svg>
+                <span>Settings</span>
+              </a>
+            </li>
+            <li>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                className="flex items-center p-2 space-x-3 rounded-md"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  className="w-5 h-5 fill-current dark:text-gray-400"
+                >
+                  <path d="M440,424V88H352V13.005L88,58.522V424H16v32h86.9L352,490.358V120h56V456h88V424ZM320,453.642,120,426.056V85.478L320,51Z"></path>
+                  <rect width="32" height="64" x="256" y="232"></rect>
+                </svg>
+                <span>Logout</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,113 +1,190 @@
-import Image from 'next/image'
+"use client";
+import Header from "./components/header";
+import SideBar from "./components/sidebar";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
+const itemList = [
+  {
+    label: "Gender",
+  },
+  {
+    label: "Age",
+  },
+  {
+    label: "Condition",
+  },
+  {
+    label: "Location",
+  },
+  {
+    label: "Price",
+  },
+];
+
+const sections = [
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "location.png",
+    location: "Kasoa",
+    availability: "weekday",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "location.png",
+    location: "Kasoa",
+    availability: "weekday",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <Header />
+      <SideBar />
+
+      <main className="min-h-screen md:pl-72 pt-10 bg-white">
+        <nav className="w-full flex flex-col  items-center md:flex-row justify-center md:justify-between">
+          <div className="md:flex items-center">
+            {itemList.map((item, index) => (
+              <ul key={index} className="flex items-center">
+                <li>{item.label}</li>
+                <ChevronDownIcon
+                  className="h-5 w-5 flex-none text-gray-400 md:mr-3"
+                  aria-hidden="true"
+                />
+              </ul>
+            ))}
+          </div>
+          <div className="pt-2 relative text-gray-600 md:pt-0">
+            <input
+              className="border-2 border-gray-300 bg-white h-10 px-5 pl-16 rounded-lg text-sm focus:outline-none"
+              type="search"
+              name="search"
+              placeholder="Search"
             />
-          </a>
+          </div>
+        </nav>
+
+        {/* body aspect */}
+        <div className="grid md:grid-cols-2  place-content-center  gap-2 sm:grid-cols-2 lg:grid-cols-4">
+          {sections.map((section) => (
+            <section
+              key={section.id}
+              className="card p-4 w-60 h-80 mt-10 bg-white rounded-2xl border border-blue-700 shadow-lg"
+            >
+              <div className="text-amber-500 text-xs font-bold text-right underline leading-relaxed">
+                {section.title}
+              </div>
+              <img
+                className="w-24 h-24 rounded-full mx-auto mt-2"
+                src={section.imageUrl}
+                alt="Portrait"
+              />
+              <div className="text-black text-xl font-bold font-inter mt-2 text-center">
+                {section.name}
+              </div>
+              <div className="text-black text-opacity-60 text-lg font-normal font-poppins leading-relaxed text-center">
+                {section.role}
+              </div>
+              <img
+                className="w-6 h-6 mx-auto mt-1"
+                src={section.locationIcon}
+                alt="Location Icon"
+              />
+              <div className="text-black text-base font-normal font-poppins leading-relaxed text-center">
+                {section.location}
+              </div>
+              <div className="mt-1 text-center">
+                <span className="text-black text-opacity-60 text-base font-normal font-poppins leading-relaxed">
+                  Availability:{" "}
+                </span>
+                <span className="text-black text-base font-normal font-poppins leading-relaxed">
+                  {section.availability}
+                </span>
+              </div>
+              <div className="text-green-800 text-base font-bold font-inter mt-1 text-center">
+                {section.rate}
+              </div>
+            </section>
+          ))}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </>
+  );
 }
