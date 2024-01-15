af7be97f87683ce3d49ec7565cafc00a424b57cd
Joshua Ntow Opare-Boateng <109949311+jnopareboateng@users.noreply.github.com>
2024-01-15 05:04:31 +0000

    Add files via upload

---
+++ app/found/page.js
@@ -0,0 +1,32 @@
+"use client";
+import Link from "next/link";
+import Header from "../../components/header";
+import SideBar from "../../components/sidebar";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+export default function Found() {
+  return (
+    <>
+      <Header />
+      <SideBar />
+      <main className="min-h-screen md:pl-72 pt-10 bg-white">
+        <section className="flex flex-col justify-center items-center">
+          <h1 className="text-2xl font-bold"> Handsome Musah </h1>
+          <img className="w-5 h-5 mt-5" src="/global.jpg" alt="global"></img>
+          <img className="w-40 h-50 mt-5" src="/user.png" alt="user"></img>
+          <Link href={"/pages/call"}>
+            <button className="relative bg-blue-700 rounded-[80px] w-60 flex flex-row items-center mt-10 justify-center p-4 text-white box-border text-center text-[20]">
+              <div className="relative">Talk to Musah</div>
+            </button>
+          </Link>
+          <span className="text-[10px] flex mt-10">
+            Filter search by gender:
+            <ChevronDownIcon
+              className="h-5 w-5 flex-none text-gray-400"
+              aria-hidden="true"
+            />
+          </span>
+        </section>
+      </main>
+    </>
+  );
+}
---
+++ app/therapists/page.js
@@ -0,0 +1,190 @@
+"use client";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import Header from "../../components/header";
+import SideBar from "../../components/sidebar";
+
+const itemList = [
+  {
+    label: "Gender",
+  },
+  {
+    label: "Age",
+  },
+  {
+    label: "Condition",
+  },
+  {
+    label: "Location",
+  },
+  {
+    label: "Price",
+  },
+];
+
+const sections = [
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "/location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "/location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "/location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "/location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "/location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "/location.png",
+    location: "Kasoa",
+    availability: "weekday",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "/location.png",
+    location: "Kasoa",
+    availability: "weekday",
+    rate: "$10/hr",
+  },
+  {
+    id: 1,
+    title: "Book now",
+    imageUrl: "https://source.unsplash.com/100x100/?portrait",
+    name: "Handsome Musah",
+    role: "Psychologist",
+    locationIcon: "/location.png",
+    location: "Cape Coast",
+    availability: "weekends",
+    rate: "$10/hr",
+  },
+];
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <SideBar />
+
+      <main className="min-h-screen md:pl-72 pt-10 bg-white">
+        <nav className="w-full flex flex-col  items-center md:flex-row justify-center md:justify-between">
+          <div className="md:flex items-center">
+            {itemList.map((item, index) => (
+              <ul key={index} className="flex items-center">
+                <li>{item.label}</li>
+                <ChevronDownIcon
+                  className="h-5 w-5 flex-none text-gray-400 md:mr-3"
+                  aria-hidden="true"
+                />
+              </ul>
+            ))}
+          </div>
+          <div className="pt-2 relative text-gray-600 md:pt-0">
+            <input
+              className="border-2 border-gray-300 bg-white h-10 px-5 pl-16 rounded-lg text-sm focus:outline-none"
+              type="search"
+              name="search"
+              placeholder="Search"
+            />
+          </div>
+        </nav>
+
+        {/* body aspect */}
+        <div className="grid md:grid-cols-2  place-content-center  gap-2 sm:grid-cols-2 lg:grid-cols-4">
+          {sections.map((section) => (
+            <section
+              key={section.id}
+              className="card p-4 w-60 h-80 mt-10 bg-white rounded-2xl border border-blue-700 shadow-lg"
+            >
+              <div className="text-amber-500 text-xs font-bold text-right underline leading-relaxed">
+                {section.title}
+              </div>
+              <img
+                className="w-24 h-24 rounded-full mx-auto mt-2"
+                src={section.imageUrl}
+                alt="Portrait"
+              />
+              <div className="text-black text-xl font-bold font-inter mt-2 text-center">
+                {section.name}
+              </div>
+              <div className="text-black text-opacity-60 text-lg font-normal font-poppins leading-relaxed text-center">
+                {section.role}
+              </div>
+              <img
+                className="w-6 h-6 mx-auto mt-1"
+                src={section.locationIcon}
+                alt="Location Icon"
+              />
+              <div className="text-black text-base font-normal font-poppins leading-relaxed text-center">
+                {section.location}
+              </div>
+              <div className="mt-1 text-center">
+                <span className="text-black text-opacity-60 text-base font-normal font-poppins leading-relaxed">
+                  Availability:{" "}
+                </span>
+                <span className="text-black text-base font-normal font-poppins leading-relaxed">
+                  {section.availability}
+                </span>
+              </div>
+              <div className="text-green-800 text-base font-bold font-inter mt-1 text-center">
+                {section.rate}
+              </div>
+            </section>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
