35b2ddfb232bc4ecd155c0c4c0d7e5ae32eababa
Joshua Ntow Opare-Boateng <109949311+jnopareboateng@users.noreply.github.com>
2024-01-15 09:14:40 +0000

    Prepare for deployment

--- app/(components)/header.js
+++ app/(components)/header.js
@@ -2,20 +2,16 @@
 import { useState } from "react";
 import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import {
-  // ArrowPathIcon,
-  Bars3Icon,
-  // ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  // SquaresPlusIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
-import {
   ChevronDownIcon,
   PhoneIcon,
   PlayCircleIcon,
-} from "@heroicons/react/20/solid";
-
+} from "@heroicons/react/solid";
+import {
+  Bars3Icon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  XMarkIcon,
+} from "@heroicons/react/outline";
 const contact_drop = [
   {
     name: "Engagement",
