16b128ad7d03f9c9281f34de5a2085c76e975a96
jnopareboateng <jnopareboateng@outlook.com>
2024-01-15 12:26:18 +0000

    deployment-fixes

--- app/pages/call/page.js
+++ app/pages/call/page.js
@@ -1,10 +1,12 @@
 export default function Call() {
   return (
     <>
+      <h3 className="flex flex-col items-center justify-center">
+        This page is dedicated to route calling of professionals and would be
+        updated soon.
+      </h3>
       <main className="min-h-screen bg-no-repeat bg-cover bg-[url('/screen.png')] pt-10 bg-white">
-        <section className="flex flex-col justify-center items-center">
-            
-        </section>
+        <section className="flex flex-col justify-center items-center"></section>
       </main>
     </>
   );
---
+++ app/pages/symptoms/page.js
@@ -0,0 +1,39 @@
+import Footer from "@/app/(components)/Footer";
+import Header from "@/app/(components)/header";
+import Link from "next/link";
+
+export default function Symptoms() {
+  return (
+    <>
+      <Header />
+      <div className="flex h-screen items-center justify-center bg-gray-100">
+        <div className="relative rounded-lg shadow-lg p-6 bg-white w-96">
+          <h4 text-xl flex flex-col items-center justify-center>
+            Hi, how are you feeling today?
+          </h4>
+          <p className="text-gray-700 font-poppins text-sm mb-4">
+            Let's connect you to a professional.
+          </p>
+          <form
+            // onSubmit={handleSubmit}
+            className="flex items-center border rounded-full px-3 py-1"
+          >
+            <input
+              className="flex-grow text-sm text-gray-700 placeholder-gray-400 outline-none"
+              type="text"
+              placeholder="Type something here"
+            />
+            <button
+              //   href="pages/call"
+              type="submit"
+              className="text-white bg-blue-500 hover:bg-blue-700 rounded-full p-2 ml-2"
+            >
+              <a href="/pages/call">Send</a>
+            </button>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
